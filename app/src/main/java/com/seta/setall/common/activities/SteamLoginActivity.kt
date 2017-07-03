3ee7d5a5a534225abfac6d6d4206c35d85f962e7
Seta <sweater@azusasoft.com>
2017-07-03 16:42:17 +0800

    first

---
+++ app/src/main/java/com/seta/setall/common/activities/SteamLoginActivity.kt
@@ -0,0 +1,13 @@
+package com.seta.setall.common.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.seta.setall.R
+
+class SteamLoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_steam_login)
+    }
+}
---
+++ app/src/main/java/com/seta/setall/common/framework/BaseApplication.kt
@@ -0,0 +1,13 @@
+package com.seta.setall.common.framework
+
+import android.app.Application
+
+/**
+ * Created by SETA_WORK on 2017/7/3.
+ */
+class BaseApplication:Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
