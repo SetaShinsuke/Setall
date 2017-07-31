2debdc890686763c932b0cc3067b8b792b15d991
Seta <sweater@azusasoft.com>
2017-07-31 18:16:27 +0800

    GameDlcPackPresenter编写中

--- app/src/main/java/com/seta/setall/common/extensions/AllExtensions.kt
+++ app/src/main/java/com/seta/setall/common/extensions/AllExtensions.kt
@@ -3,12 +3,14 @@ package com.seta.setall.common.extensions
 import android.util.Log
 import com.seta.setall.common.mvp.BasePresenter
 import com.seta.setall.common.mvp.MvpView
-import java.text.SimpleDateFormat
-import java.util.*
 
 /**
  * Created by SETA_WORK on 2017/7/4.
  */
 fun <T : MvpView?> BasePresenter<T>.logD(message: String) {
     Log.d(javaClass.simpleName, message)
+}
+
+fun <T : MvpView?> BasePresenter<T>.logW(message: String, throwable: Throwable? = null) {
+    Log.w(javaClass.simpleName, message, throwable)
 }
--- app/src/main/java/com/seta/setall/steam/presenters/GameDlcPackPresenter.kt
+++ app/src/main/java/com/seta/setall/steam/presenters/GameDlcPackPresenter.kt
@@ -1,12 +1,12 @@
 package com.seta.setall.steam.presenters
 
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
 import com.seta.setall.common.extensions.logD
+import com.seta.setall.common.extensions.logW
 import com.seta.setall.common.http.Network
 import com.seta.setall.common.mvp.BasePresenter
 import com.seta.setall.steam.api.models.GameDetailBean
 import com.seta.setall.steam.mvpViews.GameDlcPackMvpView
+import com.seta.setall.steam.utils.SteamUtilMethods
 import rx.Observable
 import rx.Subscriber
 
@@ -18,53 +18,42 @@ class GameDlcPackPresenter : BasePresenter<GameDlcPackMvpView>() {
     fun loadGameDlcPack(gameId: Int) {
         Network.steamGameApi.getGameDetail(gameId)
                 .map {
-                    val gson = Gson()
-                    val json = gson.toJsonTree(it).asJsonObject
-                    val data = json.entrySet().iterator().next().toPair().second.asJsonObject["data"]
-                    val gameDetailBean = gson.fromJson<GameDetailBean>(data, GameDetailBean::class.java)
-                    logD("Game name : ${gameDetailBean.name}")
-                    return@map gameDetailBean
+                    return@map SteamUtilMethods.createGameDetailBean(it)
                 }.doSubscribe(object : Subscriber<GameDetailBean>() {
-            override fun onCompleted() {
-                logD("ReqGetGame onCompleted")
+            override fun onNext(t: GameDetailBean?) {
+                logD("LoadGame detail onNext : $t")
+                val reqGetDlc: Observable<List<GameDetailBean>>? = getReqGameDetails(t?.dlc?.toList())
             }
 
             override fun onError(e: Throwable?) {
-                logD("ReqGetGame onError : ${e?.message}")
+                logW("LoadGame detail onError : e")
             }
 
-            override fun onNext(t: GameDetailBean?) {
-                logD("ReqGetGame onNext : $t")
+            override fun onCompleted() {
+                logD("LoadGame detail onCompleted")
             }
+
         })
     }
 
-    fun loadDlcPack(gameDetailBean: GameDetailBean) {
-        val ids = gameDetailBean.dlc
-        val reqArray = ids?.map { Network.steamGameApi.getGameDetail(it) }
-        Observable.zip(reqArray) {
+    fun getReqGameDetails(gameIds: List<*>?): Observable<List<GameDetailBean>>? {
+        if (gameIds == null) {
+            return null
+        }
+        val ids = gameIds.filterIsInstance<Int>()
+        val reqArray = ids.map { Network.steamGameApi.getGameDetail(it) }
+        val observable: Observable<List<GameDetailBean>> = Observable.zip(reqArray) {
             val gameDetails = ArrayList<GameDetailBean>()
             it.forEach {
-                val gson = GsonBuilder().create()
-                val json = gson.toJsonTree(it).asJsonObject
-                val data = json.entrySet().iterator().next().toPair().second.asJsonObject["data"]
-                val gameDetailBean = gson.fromJson<GameDetailBean>(data, GameDetailBean::class.java)
-                gameDetails.add(gameDetailBean)
+                //                val gson = GsonBuilder().create()
+//                val json = gson.toJsonTree(it).asJsonObject
+//                val data = json.entrySet().iterator().next().toPair().second.asJsonObject["data"]
+//                val gameDetailBean = gson.fromJson<GameDetailBean>(data, GameDetailBean::class.java)
+//                gameDetails.add(gameDetailBean)
+                gameDetails.add(SteamUtilMethods.createGameDetailBean(it))
             }
             return@zip gameDetails
-        }.doSubscribe(object : Subscriber<ArrayList<GameDetailBean>>() {
-            override fun onError(e: Throwable?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-
-            override fun onCompleted() {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-
-            override fun onNext(t: ArrayList<GameDetailBean>?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-
-        })
+        }
+        return observable
     }
 }
